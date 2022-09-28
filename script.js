e96f94ea0204eca1f7d841e1a556a4b0925b0f54
Maurice <rentillosa90@gmail.com>
2022-09-28 19:17:13 +0800

    improved appearance a bit, made input fields as div

--- script.js
+++ script.js
@@ -1,29 +1,38 @@
-const questions = {} //object for questions
+const questions = {}; //object for questions
 
 function getQuestion() {
-   const def = document.getElementById('def')
-   const term = document.getElementById('term')
-   const submitBtn = document.getElementById('submitBtn')
-   const qCont = document.getElementById('qContainer')
+   const def = document.getElementById('def');
+   const term = document.getElementById('term');
+   const submitBtn = document.getElementById('submit-btn');
+   const qCont = document.querySelector('.questions-list');
+   //input fields
+   const inputField = document.querySelector('.input-fields');
+   const defCont = document.querySelector('.def-container');
+   const ansCont = document.querySelector('.answer-container');
 
-   let numCount = 1
+   let numCount = 1;
 
-   //event listener for submit btn
-   submitBtn.addEventListener('click', () => {
-      const qDiv = document.createElement('div')
-      qDiv.classList.add('qDiv')
-      const q = document.createElement('p')
+   //text field event listener
+   //save question to object and show on page
+   inputField.addEventListener('keydown', e => {
+      if(e.key === 'Enter') {
+         e.preventDefault();
+         if(defCont.textContent != '' && ansCont.textContent != '') {
+            const qDiv = document.createElement('div');
+            qDiv.classList.add('qDiv');
+            const q = document.createElement('p');
+            
+            q.textContent = `${numCount}. ${ansCont.textContent} - ${defCont.textContent}`;
+            questions[ansCont.textContent] = defCont.textContent;
+            defCont.textContent = '';
+            ansCont.textContent = '';
 
-      if(term.value || def.value) {
-         q.innerText = `${numCount}. ${term.value} - ${def.value}`
-         questions[term.value] = def.value;
-         def.value = ''
-         term.value = ''
-         qDiv.append(q)
-         qCont.append(qDiv)
-         numCount++
-      }
-   })
+            qDiv.append(q);
+            qCont.append(qDiv);
+            numCount++;
+         };
+      };
+   });
 }
 
 function startQuiz() {
