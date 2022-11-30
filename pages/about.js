8e89a2f6a23f10ecd490876410f67a687841d4e2
pokemau <rentillosa90@gmail.com>
2022-11-30 10:35:14 +0800

    added simple about page

--- pages/about.js
+++ pages/about.js
@@ -1,12 +1,20 @@
 import Head from "next/head";
 
+// import styles from "../styles/globals.css";
+
 const About = () => {
   return (
     <>
       <Head>
         <title>Flashcardo | About</title>
       </Head>
-      <h1>About</h1>
+      <div className="about-cont">
+        <h1>About Flashcardo</h1>
+        <p>
+          Flaschardo is a simple site that makes flashcards to help myself and
+          hopefully others in studying.
+        </p>
+      </div>
     </>
   );
 };
