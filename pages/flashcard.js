2f0135f5e8f1acf79019720e6ef23a9d225f30fa
pokemau <rentillosa90@gmail.com>
2023-02-01 23:48:35 +0800

    user can go to previous questions

--- pages/flashcard.js
+++ pages/flashcard.js
@@ -6,59 +6,64 @@ const Flashcard = () => {
   const [questionsList, setQuestionsList] = useState([]);
   const [currQuestion, setCurrQuestion] = useState("");
   const [currAns, setCurrAns] = useState("");
-  const firstQisRun = useRef(true);
   const ansRef = useRef(null);
+  const [currNum, setCurrNum] = useState(0);
 
-  // get localstorage items
+  // get local storage item
   useEffect(() => {
-    const t = localStorage.getItem("currTitle");
-    setCurrTitle(t);
-    setQuestionsList(JSON.parse(localStorage.getItem(t)));
+    const title = localStorage.getItem("currTitle");
+    let questions = JSON.parse(localStorage.getItem(title));
+    let runOnce = 0;
+
+    setCurrTitle(title);
+
+    // set questionslist items
+    while (questions.length) {
+      const randNum = Math.floor(Math.random() * questions.length);
+      const newQuestion = questions[randNum];
+
+      // set initial currQuestion and currAns
+      if (!runOnce) {
+        setCurrAns(newQuestion.ans);
+        setCurrQuestion(newQuestion.def);
+        runOnce = 1;
+      }
+
+      setQuestionsList((prevQuestions) => [...prevQuestions, newQuestion]);
+      questions = questions.filter((item) => item !== newQuestion);
+    }
   }, []);
 
-  // get first question on render
-  useEffect(() => {
-    if (firstQisRun.current && questionsList.length) {
-      getFirstQuestion();
-      firstQisRun.current = false;
-    }
-  }, [questionsList]);
+  // update currQues and currAns values
+  const updateQuestionInfo = () => {
+    setCurrQuestion(questionsList[currNum].def);
+    setCurrAns(questionsList[currNum].ans);
+    ansRef.current.innerText = "";
+  };
 
-  // get random num
-  function getRandNum() {
-    const randNum = Math.floor(Math.random() * questionsList.length);
-    return randNum;
-  }
+  // update current question text
+  useEffect(() => {
+    if (questionsList.length) updateQuestionInfo();
+  }, [currNum]);
+
+  // get next question
+  const nextQuestion = () => {
+    if (currNum + 1 < questionsList.length)
+      setCurrNum((prevNum) => prevNum + 1);
+    else alert("No more Questions");
+  };
+
+  // previous question
+  const prevQuestion = () => {
+    if (currNum > 0) setCurrNum((prevNum) => prevNum - 1);
+    else alert("This is the first question.");
+  };
 
   // show answer
-  function showAns(e) {
+  const showAns = (e) => {
     e.preventDefault();
-
     ansRef.current.innerText = currAns;
-  }
-
-  // get first question
-  function getFirstQuestion() {
-    const randNum = getRandNum();
-    if (questionsList.length) {
-      setCurrQuestion(questionsList[randNum].def);
-      setCurrAns(questionsList[randNum].ans);
-      setQuestionsList(questionsList.filter((q, index) => index !== randNum));
-    }
-  }
-
-  // get random question
-  function getRandomQuestion(e) {
-    e.preventDefault();
-    const randNum = getRandNum();
-
-    if (questionsList.length) {
-      setCurrQuestion(questionsList[randNum].def);
-      setCurrAns(questionsList[randNum].ans);
-      setQuestionsList(questionsList.filter((q, index) => index !== randNum));
-      ansRef.current.innerText = "";
-    } else window.alert("No More Questions!!");
-  }
+  };
 
   return (
     <>
@@ -67,12 +72,20 @@ const Flashcard = () => {
       </Head>
       <div className="flashcards">
         <h1 className="text-center font-bold text-2xl my-2">{currTitle}</h1>
-        <div className="text-center border-[1px] border-[#c9c9c9] rounded p-2 w-[90vw] mx-auto min-h-[10em] flex flex-col justify-between break-words">
+        <div
+          className={`text-center border-[1px] border-[#c9c9c9] rounded p-2 w-[90vw] mx-auto min-h-[10em] flex flex-col justify-between break-words`}>
           <p className="text-lg">{currQuestion}</p>
           <p className="text-lg font-bold text-[#a15dad]" ref={ansRef}></p>
         </div>
 
         <div className="flex justify-center">
+          <button
+            className="flashcard-btn px-11"
+            type="button"
+            onClick={(e) => prevQuestion(e)}>
+            Previous
+          </button>
+
           <button
             className="flashcard-btn"
             type="button"
@@ -83,7 +96,7 @@ const Flashcard = () => {
           <button
             className="flashcard-btn px-11"
             type="button"
-            onClick={(e) => getRandomQuestion(e)}>
+            onClick={(e) => nextQuestion(e)}>
             Next
           </button>
         </div>
