64897ff2b3ce91dc0e7044f0f96f002bcd472d30
pokemau <rentillosa90@gmail.com>
2023-02-26 18:06:34 +0800

    changed div to textarea, autofocus on question form

--- components/edit-questions/InputFields.js
+++ components/edit-questions/InputFields.js
@@ -13,21 +13,23 @@ const InputFields = ({
   function handleAddQues(e) {
     e.preventDefault();
 
-    const d = inputDefRef.current.innerText;
-    const a = inputAnsRef.current.innerText;
+    const d = inputDefRef.current.value;
+    const a = inputAnsRef.current.value;
 
     if (d && a) {
       setQuestionsList([...questionsList, { id: numCount, def: d, ans: a }]);
       setNumCount(numCount + 1);
 
-      inputDefRef.current.innerText = "";
-      inputAnsRef.current.innerText = "";
+      inputDefRef.current.value = "";
+      inputAnsRef.current.value = "";
+
+      inputDefRef.current.focus();
     }
   }
 
   const checkEnter = (e) => {
-    const d = inputDefRef.current.innerText;
-    const a = inputAnsRef.current.innerText;
+    const d = inputDefRef.current.value;
+    const a = inputAnsRef.current.value;
 
     if (d && a && e.key === "Enter") {
       handleAddQues(e);
@@ -37,18 +39,32 @@ const InputFields = ({
   return (
     <>
       <div className="mt-4 lg:mt-5 w-[100%] flex flex-col items-center  mb-4 lg:w-[50%] lg:mx-auto">
-        <div
+        <textarea
+          name="ques-form"
+          placeholder="Write your question here..."
+          className="block h-full min-h-[10em] resize-none w-[90%] mt-2 border-[1px] border-[#b1b1b1] rounded text-xl p-2"
+          ref={inputDefRef}
+          onKeyDown={checkEnter}></textarea>
+
+        <textarea
+          name="ans-form"
+          placeholder="Answer here..."
+          className="resize-none w-[90%] border-[1px] border-[#b1b1b1] rounded mb-4 mt-2 p-2 text-xl"
+          ref={inputAnsRef}
+          onKeyDown={checkEnter}></textarea>
+
+        {/* <div
           className="w-[90%] mt-2 min-h-[10em] border-[1px] border-[#b1b1b1] rounded text-xl p-2 empty-input"
           contentEditable="true"
           ref={inputDefRef}
           input-placeholder="Write your question here..."
-          onKeyDown={checkEnter}></div>
-        <div
+          onKeyDown={checkEnter}></div> */}
+        {/* <div
           className="empty-input w-[90%] border-[1px] border-[#b1b1b1] rounded mb-4 mt-2 p-2 text-xl"
           contentEditable="true"
           ref={inputAnsRef}
           input-placeholder="Answer here..."
-          onKeyDown={checkEnter}></div>
+          onKeyDown={checkEnter}></div> */}
         <button
           className="py-1 px-2 rounded text-lg bg-[#b989c2] hover:bg-[#a77aaf] transition-all duration-100 "
           type="button"
