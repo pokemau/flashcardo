de1b6b855756fd203bdd3914f64500ab490c4d71
pokemau <rentillosa90@gmail.com>
2023-03-19 17:18:13 +0800

    made function names clearer

--- components/HomePage/PreviousCards.js
+++ components/HomePage/PreviousCards.js
@@ -4,13 +4,14 @@ import { BsFillTrashFill } from "react-icons/bs";
 
 const PreviousCards = ({ titleSets, setTitleSets, setCurrTitle }) => {
   const router = useRouter();
+
   // select question from previous sets
-  function goToQuestion(title) {
+  function goToFlashcardSet(title) {
     setCurrTitle(title);
     router.push("/flashcard");
   }
 
-  function delPrevQuestion(title, index) {
+  function deleteFlashcardSet(title, index) {
     localStorage.removeItem(title);
     setTitleSets(
       titleSets.filter((t, tIndex) => t !== title && tIndex !== index)
@@ -24,19 +25,20 @@ const PreviousCards = ({ titleSets, setTitleSets, setCurrTitle }) => {
           titleSets.map((title, index) => (
             <div
               className="flex items-center w-[70%] h-10 rounded pr-1 border-[1px] border-[#d4d4d4] mb-2 lg:w-[30vw]"
-              key={Math.random() * 1000}>
+              key={`${title}${Math.random() * 1000}`}>
               <div
                 className="cursor-pointer flex items-center pl-4 rounded text-lg w-[90%] hover:text-[#954ca0] hover:font-bold transition-all duration-100"
                 onClick={() => {
-                  goToQuestion(title);
+                  goToFlashcardSet(title);
                 }}>
                 <p>{title}</p>
               </div>
+
               <button
                 className="question-list-btn"
                 type="button"
                 onClick={() => {
-                  delPrevQuestion(title, index);
+                  deleteFlashcardSet(title, index);
                 }}>
                 <BsFillTrashFill />
               </button>
--- pages/index.js
+++ pages/index.js
@@ -3,12 +3,18 @@ import Head from "next/head";
 import { useState, useEffect } from "react";
 
 // components
-import PreviousCards from "../components/index/PreviousCards";
-import CreateNewCard from "../components/index/CreateNewCard";
+import PreviousCards from "../components/HomePage/PreviousCards";
+import CreateNewCard from "../components/HomePage/CreateNewCard";
 
 export default function Home() {
   const [currTitle, setCurrTitle] = useState("");
   const [titleSets, setTitleSets] = useState(null);
+
+  const testBtn = (e) => {
+    e.preventDefault();
+
+    console.log(titleSets);
+  };
 
   // update titlesets if a set is deleted
   useEffect(() => {
@@ -33,6 +39,7 @@ export default function Home() {
         <title>Flashcardo | Home</title>
       </Head>
       <div className="w-[100vw] my-5 mx-auto">
+        <button onClick={testBtn}>SHOW TITLESETS</button>
         <CreateNewCard currTitle={currTitle} setCurrTitle={setCurrTitle} />
 
         <PreviousCards
