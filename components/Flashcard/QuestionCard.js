9cf55bc69ff8939d91e3b31791188c2da3cd1c55
pokemau <rentillosa90@gmail.com>
2023-03-26 16:26:46 +0800

    shorten flashcard witdh and fix word wrap bug

--- components/Flashcard/QuestionCard.js
+++ components/Flashcard/QuestionCard.js
@@ -1,7 +1,7 @@
 const QuestionCard = ({ showAns, currAns, currQuestion }) => {
   return (
     <div className="relative w-[80vw] h-[40vh] mx-auto mb-2 text-center ">
-      <div className="card w-full h-full rounded mx-auto min-h-[10em] break-words">
+      <div className="card w-full h-full lg:w-[70%] rounded mx-auto min-h-[10em]">
         <div className={`card-inner ${showAns ? "card-flip" : ""}`}>
           <div className="card-front absolute w-full h-full text-2xl bg-orange-300">
             <div className="absolute text-base top-1 left-1 text-gray-600">
