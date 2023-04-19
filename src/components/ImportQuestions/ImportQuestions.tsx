d34f93ace4baf05d6612feaa4716908316d4a9b4
pokemau <rentillosa90@gmail.com>
2023-04-19 13:47:06 +0800

    move /components /pages/ /styles to /src

--- src/components/ImportQuestions/ImportQuestions.tsx
+++ src/components/ImportQuestions/ImportQuestions.tsx
@@ -3,15 +3,17 @@ import { QuestionsListType } from "../../pages/edit";
 interface ImportQuestionsProps {
   title: string;
   questions: QuestionsListType[];
+  titleSets: string[];
 }
 
 const ImportQuestions: React.FC<ImportQuestionsProps> = ({
   title,
   questions,
+  titleSets,
 }) => {
   return (
     <div>
-      <p>{title}</p>
+      <p>You are black</p>
     </div>
   );
 };
