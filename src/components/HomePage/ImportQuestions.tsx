033cbdf5ee6b56d1a234c23f9fc3dbc5c4ecada4
pokemau <rentillosa90@gmail.com>
2023-04-19 19:41:16 +0800

    add placeholder text to textarea when importing

--- src/components/HomePage/ImportQuestions.tsx
+++ src/components/HomePage/ImportQuestions.tsx
@@ -7,6 +7,8 @@ type ImportQuestionsProps = {
 const ImportQuestions: React.FC<ImportQuestionsProps> = ({
   setImportQuestions,
 }) => {
+  const divPlaceholder = "Enter exported questions here...";
+
   return (
     <div className="w-[65%] md:w-[50%] lg:w-[30%]">
       <div
@@ -14,9 +16,11 @@ const ImportQuestions: React.FC<ImportQuestionsProps> = ({
         role="textbox"
         contentEditable={true}
         suppressContentEditableWarning={true}
-        onInput={(e) =>
-          setImportQuestions(e.currentTarget.textContent || "")
-        }></div>
+        onFocus={(e) => (e.currentTarget.textContent = "")}
+        onBlur={(e) => (e.currentTarget.textContent = divPlaceholder)}
+        onInput={(e) => setImportQuestions(e.currentTarget.textContent || "")}>
+        {divPlaceholder}
+      </div>
     </div>
   );
 };
