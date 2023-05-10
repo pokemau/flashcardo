d2d8b8c6fe2e2a9636992df2f4d5513b36a76432
pokemau <rentillosa90@gmail.com>
2023-05-10 22:30:15 +0800

    change icon color

--- src/components/jorash-mode/JorashMode.tsx
+++ src/components/jorash-mode/JorashMode.tsx
@@ -74,13 +74,16 @@ const JorashMode: React.FC<JorashModeProps> = ({ setQuestionsList }) => {
         {!isValidInput && <InvalidMessage msg={invalidFormatMsg} />}
 
         <div className="flex justify-center items-center">
-          <div className="flex flex-col items-center gap-2 ">
+          <div className="flex flex-col items-center ">
             <Button
               btnFunc={() => addQuestions(userInput)}
               btnTitle="Add Questions"
             />
             <div className="group relative inline-block">
-              <AiOutlineQuestionCircle className="cursor-pointer text-[1.5rem]" />
+              <AiOutlineQuestionCircle
+                className="cursor-pointer text-[1.5rem]
+              text-[#797979] font-[300]"
+              />
 
               <div
                 className="invisible group-hover:visible opacity-0 
