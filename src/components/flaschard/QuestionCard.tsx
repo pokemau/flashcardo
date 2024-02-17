fe9cb901ebbcf70451ed3176175ee367e38c2d7d
pokemau <rentillosa90@gmail.com>
2024-02-17 22:50:57 +0800

    fix div stuff

--- src/components/flaschard/QuestionCard.tsx
+++ src/components/flaschard/QuestionCard.tsx
@@ -1,27 +1,34 @@
+import { Dispatch, SetStateAction } from "react";
+
 type QuestionCardProps = {
   showAns: boolean;
+  setShowAns: Dispatch<SetStateAction<boolean>>;
   currAns: string;
   currQuestion: string;
 };
 
 const QuestionCard: React.FC<QuestionCardProps> = ({
   showAns,
+  setShowAns,
   currAns,
   currQuestion,
 }) => {
   return (
-    <div
-      className="relative w-[80vw] lg:w-[60vw] h-[40vh] mx-auto 
-    mb-2 text-center">
+    <>
+      {/* CARD */}
       <div
-        className="bg-transparent w-full h-full rounded mx-auto
-      min-h-[10em] card">
+        onClick={() => setShowAns((prevShowAns) => !prevShowAns)}
+        className="card bg-transparent
+        m-auto relative w-[90vw] lg:w-[60vw]">
+        {/* INNER */}
         <div
-          className={`card-inner relative w-full h-full text-center
-        ${showAns ? "card-flip" : ""}`}>
+          className={`card-inner relative text-center ${
+            showAns ? "card-flip" : ""
+          } relative h-[65vh] md:h-]55vh\ rounded-md text-sm lg:text-xl`}>
+          {/* FRONT */}
           <div
-            className="card-front absolute w-full h-full text-2xl 
-          bg-orange-300">
+            className="card-front absolute bg-[#c9af3f] flex items-center
+            justify-center rounded-md p-1">
             <p className="absolute text-base top-1 left-1 text-gray-600">
               Question
             </p>
@@ -29,7 +36,10 @@ const QuestionCard: React.FC<QuestionCardProps> = ({
             <p>{currQuestion}</p>
           </div>
 
-          <div className="card-back w-full h-full text-2xl bold bg-green-300">
+          {/* BACK */}
+          <div
+            className="card-back absolute bg-green-300 flex items-center
+            justify-center rounded-md p-1">
             <p className="absolute text-base top-1 left-1 text-gray-600">
               Answer
             </p>
@@ -38,7 +48,37 @@ const QuestionCard: React.FC<QuestionCardProps> = ({
           </div>
         </div>
       </div>
-    </div>
+
+      {/* <div
+        className="relative w-[80vw] lg:w-[60vw] bg-red-200 h-[40vh] mx-auto 
+        mb-2 text-center">
+        <div
+          className="bg-green-600 w-full h-full rounded mx-auto
+          min-h-[10em] card">
+          <div
+            className={`card-inner relative w-full h-full text-center
+        ${showAns ? "card-flip" : ""}`}>
+            <div
+              className="card-front absolute w-full h-[40vh] text-2xl 
+            bg-[#c9af3f]">
+              <p className="absolute text-base top-1 left-1 text-gray-600">
+                Question
+              </p>
+
+              <p className="break-all">{currQuestion}</p>
+            </div>
+
+            <div className="card-back w-full h-full text-2xl bold bg-green-300">
+              <p className="absolute text-base top-1 left-1 text-gray-600">
+                Answer
+              </p>
+
+              <p>{`${showAns ? currAns : ""}`}</p>
+            </div>
+          </div>
+        </div>
+      </div> */}
+    </>
   );
 };
 
